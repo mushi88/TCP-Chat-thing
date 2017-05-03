fad303a1f796e869634e454fcefd4f7955d98318
Kirsi <mushi88@users.noreply.github.com>
2017-05-04 08:52:20 +1000

    Added Port Scanner

--- Stuff/Connection.py
+++ Stuff/Connection.py
@@ -1,6 +1,6 @@
 import socket
 
-class Connect():
+class Connect:
 	def __init__(self, Port=2345, IP="localhost", Buffer=10240):
 		sock=socket.socket(AF_INET, SOCK_STREAM)
 		self.sock=sock
@@ -24,4 +24,40 @@ class Connect():
 			return Data
 		except:
 			return "Failed to recieve data."
+	
+class PortScan:
+	def __init__(self, HostIP="localhost"):
+		self.HostIP=HostIP
+	
+	def Go(self)
+		threads=[]
+		output={}
+		ports=[]
+		
+		for x in range(2000,5000):
+			t=threading.Thread(target=self.Connect, args=(self.HostIP, x, output))
+			threads.append(t)
+		
+		for x in range(2000,5000):
+			threads[x].start()
+		
+		for x in range(2000,5000):
+			threads[x].join()
+			
+		for x in range(2000,5000):
+			if not output[x]==None:
+				ports.append(output[x])
+		
+        return ports
+    
+    def Connect(self, IP, x, output):
+        PortScan=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        PortScan.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        TCPsock.settimeout(1)
+        try:
+            PortScan.connect((IP, x))
+            output[x]=str(x)
+        except:
+            outut[x]=None
+        
 	
